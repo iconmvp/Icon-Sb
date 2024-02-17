3203c2b214f6d5bd9058732638a1543b504cf5d6
iconmvp <152302809+iconmvp@users.noreply.github.com>
2024-02-17 10:46:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 import discord
 import json
 import asyncio
+import os
+from discord.ext import commands
+
+
+
+with open('config.json', 'r') as file:
+    config_data = json.load(file)
+    token = config_data["token"]
+    prefix = config_data["prefix"]
+
+
+intents = discord.Intents.default()
+
+client = commands.Bot(prefix, intents=intents, help_command=None)
+
+@client.event
+async def on_ready():
+  print(f"[+] Logged in as {client.user.name})
+  print(f"[+] Client id : {client.user.id}")
+
+  
